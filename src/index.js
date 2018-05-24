a607a6730cef3c830e6ac40d52de7191490c64c0
Tokarev Ilia <upside2002@mail.ru>
2018-05-24 15:46:55 +1100

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import _ from 'lodash';
 import { isURL } from 'validator';
+import $ from 'jquery';
 
 const parser = new DOMParser();
 const form = document.querySelector('form');
@@ -10,14 +11,26 @@ const input = document.querySelector('input');
 const submit = document.querySelector('.btn-primary');
 const state = {
   feeds: [],
-  articles: [],
+  addedFeedsList: [],
+  valid: false,
 };
-const listAdded = [];
-const createTree = () => {
+
+const extractData = (feed) => {
+  const channel = parser.parseFromString(feed, 'application/xml').querySelector('channel');
+  const [title, description] = channel.childNodes;
+  const items = channel.querySelectorAll('item');
+  return { title, description, items };
+};
+const showModal = (description) => {
+  const modalBody = document.querySelector('.modal-body > p');
+  modalBody.textContent = description.textContent;
+  $('.modal').modal('show');
+};
+const displayData = () => {
   const listFeeds = document.querySelector('.list-feeds > ul');
   const listArticles = document.querySelector('.list-articles > ul');
-  [...listArticles.childNodes].forEach(child => child.remove());
-  [...listFeeds.childNodes].forEach(child => child.remove());
+  [...listArticles.childNodes].forEach(child => child.remove()); // !!!!
+  [...listFeeds.childNodes].forEach(child => child.remove());// !!!!
   state.feeds.forEach((feed) => {
     const li = document.createElement('li');
     const h3 = document.createElement('h3');
@@ -27,17 +40,24 @@ const createTree = () => {
     li.appendChild(h3);
     li.appendChild(p);
     listFeeds.appendChild(li);
-  });
-  state.articles.forEach((item) => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    const title = item.querySelector('title');
-    const link = item.querySelector('link').textContent;
-    a.textContent = title.textContent;
-    a.href = link;
-    li.appendChild(a);
-    listArticles.appendChild(li);
-    input.value = '';
+    feed.items.forEach((item) => {
+      const li2 = document.createElement('li');
+      const a = document.createElement('a');
+      const title = item.querySelector('title');
+      const link = item.querySelector('link').textContent;
+      const button = document.createElement('input');
+      const description = item.querySelector('description');
+      button.setAttribute('class', 'btn btn-primary');
+      button.setAttribute('type', 'submit');
+      button.setAttribute('value', 'description');
+      button.addEventListener('click', () => showModal(description));
+      a.textContent = title.textContent;
+      a.href = link;
+      li2.appendChild(a);
+      li2.appendChild(button);
+      listArticles.appendChild(li2);
+      input.value = '';
+    });
   });
 };
 
@@ -51,13 +71,10 @@ const getData = (e) => {
       submit.removeAttribute('disabled');
       const span = document.querySelector('#err');
       span.textContent = '';
-      const channel = parser.parseFromString(res.data.body, 'application/xml').querySelector('channel');
-      const [title, description] = channel.childNodes;
-      const items = channel.querySelectorAll('item');
-      state.feeds = [...state.feeds, { title, description }];
-      state.articles = [...state.articles, ...items];
-      listAdded.push(adress);
-      createTree();
+      const obj = extractData(res.data.body);
+      state.feeds.push(obj);
+      state.addedFeedsList.push(adress);
+      displayData();
     })
     .catch(() => {
       const span = document.querySelector('#err');
@@ -65,14 +82,18 @@ const getData = (e) => {
     });
 };
 
+const isUrlValid = str => (isURL(str) && !state.addedFeedsList.includes(str));
+
 const checkUrl = (e) => {
   const str = e.target.value;
-  if ((str && !isURL(str)) || listAdded.includes(str)) {
+  if (!isUrlValid(str)) {
     e.target.setAttribute('style', 'border-color: red');
     submit.setAttribute('disabled', 1);
+    state.valid = false;
   } else {
     e.target.setAttribute('style', 'border-color: #80bdff');
     submit.removeAttribute('disabled');
+    state.valid = true;
   }
 };
 input.addEventListener('input', checkUrl);
