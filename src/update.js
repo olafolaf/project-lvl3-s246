590678778fdc8641336136faaa2a7842c295f1a2
Tokarev Ilia <upside2002@mail.ru>
2018-05-27 23:17:31 +1100

    fixed update.js

--- src/update.js
+++ src/update.js
@@ -3,19 +3,19 @@ import render from './render';
 import parseRss from './parser';
 
 const corsProxy = 'https://cors-proxy.htmldriven.com/';
-
+const findNewItems = (obj, items) => obj.items.filter((item2) => {
+  const title2 = item2.title;
+  return [...items].every((item1) => {
+    const title1 = item1.title;
+    return title1.textContent !== title2.textContent;
+  });
+});
 const update = (input, state) => {
   state.feeds.forEach(({ adress, items }) => {
     axios.get(`${corsProxy}?url=${adress}`)
       .then((res) => {
         const obj = parseRss(res.data.body);
-        const newItems = obj.items.filter((item2) => {
-          const title2 = item2.title;
-          return [...items].every((item1) => {
-            const title1 = item1.title;
-            return title1.textContent !== title2.textContent;
-          });
-        });
+        const newItems = findNewItems(obj, items);
         if (newItems.length > 0) {
           items.push(...newItems);
           console.log(items);
